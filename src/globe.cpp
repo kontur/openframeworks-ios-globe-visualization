d75edd26e13060de141c188f6ef4c11e5cd9b77d
Johannes Neumeier <hello@johannesneumeier.com>
2015-06-08 16:15:46 +0300

    finally figured out a fix to the transparency rendering issue... open gl backface culling to the rescue

--- src/globe.cpp
+++ src/globe.cpp
@@ -23,24 +23,23 @@ void globe::draw() {
     
     ofSetColor(255);
     
-    sphere.enableNormals();
+    //sphere.enableNormals();
     sphere.enableTextures();
     
-    ofTexture tex = textureImage.getTextureReference();
-    
-    //tex.bind();
-    sphere.mapTexCoordsFromTexture(tex);
-    //tex.draw(0, 0);
-    tex.bind();
+    textureImage.getTextureReference().bind();
     
     ofRotateX(180);
+    
+    // enable solid surface and backface culling in Open GL
+    glEnable(GL_CULL_FACE); // Cull back facing polygons
+    glCullFace(GL_FRONT);
     
     sphere.draw();
     textureImage.unbind();
     
     
     ofSetColor(150, 255, 100);
-    sphere.drawNormals(5);
+    //sphere.drawNormals(5);
     sphere.drawAxes(150);
     
     ofSetLineWidth(0.5);
