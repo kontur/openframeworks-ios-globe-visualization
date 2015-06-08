60f3173c3f4110daa985786efe751dcd6672e699
Johannes Neumeier <hello@johannesneumeier.com>
2015-06-08 17:51:07 +0300

    added depthbuffer and retina support to main setup

--- src/main.mm
+++ src/main.mm
@@ -2,7 +2,12 @@
 #include "ofApp.h"
 
 int main(){
-	ofSetupOpenGL(1024,768,OF_FULLSCREEN);			// <-------- setup the GL context
+	//ofSetupOpenGL(1024,768,OF_FULLSCREEN);			// <-------- setup the GL context
 
+    ofAppiOSWindow * iOSWindow = new ofAppiOSWindow();
+    ofSetupOpenGL(iOSWindow, 1024, 768, OF_FULLSCREEN);
+    iOSWindow->enableRetina();
+    iOSWindow->enableDepthBuffer();
+    
 	ofRunApp(new ofApp());
 }
